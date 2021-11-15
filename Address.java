5aaa31e93b0f4c4950290af2fdc5a13c8e3006e2
100382467 <56141328+willscomp@users.noreply.github.com>
2021-11-14 22:17:59 -0800

    Update Address.java 01

--- Address.java
+++ Address.java
@@ -11,28 +11,44 @@ public class Address {
 	String fullAddrees;
 	
 	public Address() {
-	
 	}
 	
 	public Address(String streetNumber, String city, String state, String contry, String postCode) {
 		this.fullAddrees = (streetNumber + " " + city + " " + state + " " + contry + " " + postCode); 
 	}
 	
-	public void getStreetNumber(String streetNumber){
+	public void setStreetNumber(String streetNumber){
 		this.streetNumber = streetNumber;
 	}
-	public void getCity(String city) {
+	public void setCity(String city) {
 		this.city = city; 
 	}
 	
-	public void getState(String state) {
+	public void setState(String state) {
 		this.state = state ; 
 	}
-	public void getCounty(String country) {
+	public void setCounty(String country) {
 		this.country = country ; 
 	}
-	public void getPostCode(String postCode) {
+	public void setPostCode(String postCode) {
 		this.postCode = postCode ; 
+	}
+	
+	public String getStreetNumber(String streetNumber){
+		return streetNumber;
+	}
+	public String getCity(String city) {
+		return city; 
+	}
+	
+	public String getState(String state) {
+		return state ; 
+	}
+	public String getCounty(String country) {
+		return country ; 
+	}
+	public String getPostCode(String postCode) {
+		return postCode ; 
 	}
 	
 	
