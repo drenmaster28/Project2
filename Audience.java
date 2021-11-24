215fdb92550b014234dd54ddf13085916b4552d6
100382467 <56141328+willscomp@users.noreply.github.com>
2021-11-23 21:46:41 -0800

    Update Audience.java

--- Audience.java
+++ Audience.java
@@ -1,32 +1,33 @@
 package audience.information;
 
+import java.util.ArrayList;
+import java.util.*;
+
 public class Audience extends Persons {
 	long id;
 	char section;
 	long seatNumber;
 	String phone;
-	Address getAddress = new Address();
+	Address address;
 	double price;
 	String offer;
 	
+	Scanner input = new Scanner(System.in);
 	
 	public Audience(String fName, String lName) {
 		super(fName, lName);
 		// TODO Auto-generated constructor stub
 	}
 	// enter complete audience data
-	public Audience(String fName, String lName,  long id, String phone, Address Address) {
+	public Audience(String fName, String lName, String phone) {
 		super(fName, lName);
-		this.id = id;
 		this.phone = phone;
-		this.getAddress = Address;
 	}
 	
-	public void setId(long id) {
-		this.id = id;
+	public void setId() {
+		this.id = (long)Math.random()*1000000000;
 	}
 	public void setSection(char section) {
-	//add if (check valid section)
 		this.section = section;
 		
 	}
@@ -36,9 +37,13 @@ public class Audience extends Persons {
 	public void setphone(String phone) {
 		this.phone = phone;
 	}
-	public void setPrice(double price) {
-		this.price = price;
-		// set 3 differnet price in method
+	public void setPrice(char section) {
+		if (section == 'a') {
+			this.price = 59;	
+		}else if(section == 'b') {
+			this.price = 72;
+		}else if(section == 'c') 
+			this.price = 88;
 	}
 	public void setOffer(String offer) {
 		this.offer = offer;
@@ -62,19 +67,54 @@ public class Audience extends Persons {
 		return offer;
 	}
 
+	//check if Section is valid
+	public boolean isValidSection() {
+		if (section == 'a') {
+			return true;
+		}else if(section == 'b') {
+			return true;
+		}else if(section == 'c') {
+			return true;
+		}
+		return false;
+	}
 	
+	//asks the user to enter all the details of an audience then assigns the values to the data members
+	public void inputDetail(){
+		super.setFName(input.next());
+		super.setLName(input.next());
+		Address address = new Address();
+		address.setStreetNumber(input.next());
+		address.setCity(input.next());
+		address.setState(input.next());
+		address.setCountry(input.next());
+		address.setPostCode(input.next());
+		
+		setphone(input.next());
+		setOffer(input.next());
+		setSection(input.next().charAt(0));
+		
+		System.out.println();
+		setOffer(input.next());
+		
+	}
 	
+	//prints the details of the audience (Id number, name, section, seat number, phone, price, if any special offer applied and total price)
 	public void issueTicket() {
 	
-		// input detail (Id number, name, section, seat number, phone, price, if any special offer applied and total price).
+		System.out.println("Id number: " + getId());
+		System.out.println("Name: " + super.getFullName());
+		System.out.println("Section: " + getSection());
+		System.out.println("Seat number: " + getSeatNumber());
+		System.out.println("Phone: " + getPhone());
+		System.out.println("Price: " + getPrice());
+		System.out.println("Discount: " + getOffer());
+		System.out.println("Total Price: " + (getPrice() + (getPrice()* Double.parseDouble(getOffer()))) );
+	
 	}
 	// check valid section 
-	
-	
-	//public 
-	
-	
-	
-	
+	public void setSpecialOffer() {
+		
+	}	
 	
 }
