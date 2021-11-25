bfc6e253fcfc053e53059e2e2732551d2c840c34
100382467 <56141328+willscomp@users.noreply.github.com>
2021-11-24 21:38:49 -0800

    Update Address.java

--- Address.java
+++ Address.java
@@ -25,35 +25,47 @@ public class Address{
 	public String getStreetNumber() {
 		return streetNumber;
 	}
+
 	public void setStreetNumber(String streetNumber) {
 		this.streetNumber = streetNumber;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getPostCode() {
 		return postCode;
 	}
+
 	public void setPostCode(String postCode) {
 		this.postCode = postCode;
 	}
+
 	public String getFullAddress() {
 		return (streetNumber + " " + city + " " + state + " " + country + " " + postCode);
 	}
 	
 	}
+
+
